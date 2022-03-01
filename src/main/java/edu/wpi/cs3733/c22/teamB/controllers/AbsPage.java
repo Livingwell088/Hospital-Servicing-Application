8fde24a42484df4c511ddc10daee04b9f2acde0f
Chris Rincon <chrisrincon23@gmail.com>
2022-03-01 14:22:57 -0500

    yay, I think this might have actually fixed the resize bug

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/AbsPage.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/AbsPage.java
@@ -13,19 +13,36 @@ public abstract class AbsPage implements IPage {
     Pane contentPane;
 
     public void initResize() {
-        contentPane.setLayoutX(Bapp.getPrimaryStage().getWidth()/4);
-        contentPane.setLayoutY(Bapp.getPrimaryStage().getHeight()/6);
+
+        //contentPane.setLayoutX(Bapp.getPrimaryStage().getWidth()/4);
+        //contentPane.setLayoutY(Bapp.getPrimaryStage().getHeight()/6);
         anchorPane.setPrefWidth(Bapp.getPrimaryStage().getWidth() - AnchorHomeController.curAnchorHomeController.sidebar.getWidth());
         anchorPane.setPrefHeight(Bapp.getPrimaryStage().getHeight() - AnchorHomeController.curAnchorHomeController.sidebar.getHeight());
+        //(anchorpane-contentane)/2
+        contentPane.setLayoutX((anchorPane.getWidth()-contentPane.getPrefWidth())/-2);
+        contentPane.setLayoutY((anchorPane.getHeight()-contentPane.getPrefHeight())/-2);
     }
 
     public void resize() {
         Bapp.getPrimaryStage().heightProperty().addListener((observable)-> {
+            //contentPane.setLayoutX(Bapp.getPrimaryStage().getWidth()/4);
+            //contentPane.setLayoutY(Bapp.getPrimaryStage().getHeight()/6);
             anchorPane.setPrefWidth(Bapp.getPrimaryStage().getWidth() - AnchorHomeController.curAnchorHomeController.sidebar.getWidth());
             anchorPane.setPrefHeight(Bapp.getPrimaryStage().getHeight() - AnchorHomeController.curAnchorHomeController.sidebar.getHeight());
-            contentPane.setLayoutX(Bapp.getPrimaryStage().getWidth()/4);
-            contentPane.setLayoutY(Bapp.getPrimaryStage().getHeight()/6);
+            //(anchorpane-contentane)/2
+           contentPane.setLayoutY((anchorPane.getPrefHeight()-contentPane.getPrefHeight())/-2);
+        });
+
+        Bapp.getPrimaryStage().widthProperty().addListener((observable)-> {
+            //contentPane.setLayoutX(Bapp.getPrimaryStage().getWidth()/4);
+            //contentPane.setLayoutY(Bapp.getPrimaryStage().getHeight()/6);
+            anchorPane.setPrefWidth(Bapp.getPrimaryStage().getWidth() - AnchorHomeController.curAnchorHomeController.sidebar.getWidth());
+            anchorPane.setPrefHeight(Bapp.getPrimaryStage().getHeight() - AnchorHomeController.curAnchorHomeController.sidebar.getHeight());
+            //(anchorpane-contentane)/2
+            contentPane.setLayoutX((anchorPane.getPrefWidth()-contentPane.getPrefWidth())/2);
         });
     }
+    }
 
-}
+
+
